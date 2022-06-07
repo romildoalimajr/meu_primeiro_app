d8c8d9e8d85a1144781a252c21f77073b8a72141
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-06-07 17:40:35 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: const MyHomePage(title: 'Frases do Dia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _numeroAleatorio = 0;
+
+  List _frases = [
+    'Não contava com minha astúcia!',
+    'Foi sem querer querendo!',
+    'Você não vai com a minha cara?',
+    'Que coisa não!'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+
+      _numeroAleatorio = new Random().nextInt(4);
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pressione o botão para gerar uma frase:',
+            ),
+            Text(
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
